cf161e0e1664dc516cee21ae8917438e12d080b7
Karim Lakhal <71038281+KarimLak@users.noreply.github.com>
2021-04-02 09:15:51 -0700

    Update main.hpp

--- INF1015Projet-H21/main.hpp
+++ INF1015Projet-H21/main.hpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+class Case {
+    public:
+    
+    private:
+    
+};
+
+class Piece {
+    public:
+    
+    private:
+    
+};
+
+class Roi : public Piece {
+    public:
+    
+    private:
+    
+};
